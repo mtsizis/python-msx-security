06ea0062a1f29c9497e6c5fab198de2c9cca9a85
mtsizis <tsizism@gmail.com>
2024-09-02 14:39:33 -0400

    Test

--- setup.py
+++ setup.py
@@ -5,11 +5,12 @@
 import os.path
 from setuptools import setup, find_packages
 
+
 setup(
     name='msxsecurity',
     version='0.1.0',
     author="Cisco MSX",
-    description="A package to exchange an MSX access token for an MSX security context, to support implementation of RBAC and tenancy in RESTful APIs.",
+    description="A package to exchange an MSX access token for an MSX security context, to support implementation of RBAC and tenancy in RESTful APIs",
     include_package_data=True,
     package_dir={"": "src"},
     packages=find_packages(),
